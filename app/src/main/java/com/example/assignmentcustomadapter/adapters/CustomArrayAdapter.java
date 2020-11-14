ea9a5edc8f8896cc4f91b3afc68886140c28de3f
mehulmangukiya9 <mehulmangukiya777@gmail.com>
2020-11-13 23:54:56 -0500

    'CustomArrayAdapter' class updated.

--- app/src/main/java/com/example/assignmentcustomadapter/adapters/CustomArrayAdapter.java
+++ app/src/main/java/com/example/assignmentcustomadapter/adapters/CustomArrayAdapter.java
@@ -1,30 +1,80 @@
 package com.example.assignmentcustomadapter.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.assignmentcustomadapter.R;
+import com.example.assignmentcustomadapter.model.Movie;
+
+import java.util.ArrayList;
 
 public class CustomArrayAdapter extends BaseAdapter {
 
+    ArrayList<Movie> movies;
+
+    public CustomArrayAdapter(ArrayList<Movie> movies)
+    {
+        this.movies = movies;
+    }
 
 
     @Override
     public int getCount() {
-        return 0;
+        return movies.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return null;
+    public Movie getItem(int position) {
+        return movies.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        ViewHolder viewHolder;
+
+        if(convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_row_layout, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Movie movie = getItem(position);
+        viewHolder.titleTv.setText(movie.getTitle());
+
+        String rating = Float.toString(movie.getRatings());
+        viewHolder.ratingsTv.setText(rating);
+
+        viewHolder.descriptionTv.setText(movie.getDescription());
+        viewHolder.movieIv.setImageResource(movie.getMovieImageId());
+
+        return convertView;
+    }
+
+    class ViewHolder{
+
+        TextView titleTv;
+        TextView ratingsTv;
+        TextView descriptionTv;
+        ImageView movieIv;
+
+        public ViewHolder(View view){
+            titleTv = view.findViewById(R.id.title_text_view);
+            ratingsTv = view.findViewById(R.id.rating_text_view);
+            descriptionTv = view.findViewById(R.id.description_text_view);
+            movieIv = view.findViewById(R.id.image_view);
+        }
     }
 }
