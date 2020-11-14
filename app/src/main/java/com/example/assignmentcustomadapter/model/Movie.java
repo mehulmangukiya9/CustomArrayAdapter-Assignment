e22eec3a140c4f46bbb2306a1b60f2bb5acccde0
mehulmangukiya9 <mehulmangukiya777@gmail.com>
2020-11-13 21:33:30 -0500

    Model 'Movie' class updated and 'CustomArrayAdapter' created.

--- app/src/main/java/com/example/assignmentcustomadapter/model/Movie.java
+++ app/src/main/java/com/example/assignmentcustomadapter/model/Movie.java
@@ -2,5 +2,47 @@ package com.example.assignmentcustomadapter.model;
 
 public class Movie
 {
+    private int movieImageId;
+    private String title;
+    private Float ratings;
+    private String description;
 
+    public Movie(int movieImageId, String title, Float ratings, String description) {
+        this.movieImageId = movieImageId;
+        this.title = title;
+        this.ratings = ratings;
+        this.description = description;
+    }
+
+    public int getMovieImageId() {
+        return movieImageId;
+    }
+
+    public void setMovieImageId(int movieImageId) {
+        this.movieImageId = movieImageId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Float ratings) {
+        this.ratings = ratings;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
